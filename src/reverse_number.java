ba18d0087e492de8c3ec2729808ee83598f45b3f
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-08-19 14:55:27 +0530

    modified:logic for reverse number

---
+++ src/reverse_number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class reverse_number {
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int number = sc.nextInt();
+        int reverse = 0;
+        while(number != 0)
+        {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number/10;
+        }
+        System.out.println("The reverse of the given number is: " + reverse);
+    }
+}
